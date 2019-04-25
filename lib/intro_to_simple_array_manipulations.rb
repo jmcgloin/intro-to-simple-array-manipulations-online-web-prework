3c04b8e7d4266ae451c5f7c2af3a764d46ed6fe3
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 23:48:18 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(ar, el)
+  ar.push(el)
+end
+
+def using_unshift(ar, el)
+  ar.unshift(el)
+end
+
+def using_pop(ar)
+  ar.pop
+end
+
+def pop_with_args(ar)
+  ar.pop(2)
+end
+
+def using_shift(ar)
+  ar.shift
+end
+
+def shift_with_args(ar)
+  ar.shift(2)
+end
+
+def using_concat(ar1, ar2)
+  ar1.concat(ar2)
+end
+
+def using_insert(ar, el)
+  ar.insert(4, el)
+end
+
+def using_uniq(ar)
+  ar.uniq
+end
+
+def using_flatten(ar)
+  ar.flatten
+end
+
+def using_delete(ar, el)
+  ar.delete(el)
+end
+
+def using_delete_at(ar, num)
+  ar.delete_at(num)
+end
